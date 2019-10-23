8396222efdcc0bb266bdc6c33a5927b2029e0657
Glenn Giordano <texasbubb@gmail.com>
2019-10-23 01:15:44 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/FTC9013/Team-9013-ftc_app-2019-2020""
    This reverts commit 68edd10bd8a5546eef5a97342f7e8239e742b1d4.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBench.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.logging.Level;
+
+@TeleOp(name="TestBench", group="Linear Opmode")
+//@Disabled
+public class TestBench extends LinearOpMode {
+
+  // Declare OpMode members.
+  private TestBenchPlatform testBench;
+  private LEDs leds;
+
+  private ElapsedTime runtime = new ElapsedTime();
+
+  @Override
+  public void runOpMode() {
+    telemetry.addData("Status", "Initialized");
+    telemetry.update();
+
+    testBench = new TestBenchPlatform(hardwareMap);
+    leds = new LEDs(hardwareMap);
+
+    // Wait for the game to start (driver presses PLAY)
+    waitForStart();
+    runtime.reset();
+
+    // run until the end of the match (driver presses STOP)
+    while (opModeIsActive())
+    {
+      // simple button press detection to call actuator test methods
+      // Some of this may be reused for TeleOp, but will likely require timer support for debounce
+      // of button presses.
+
+
+
+      if (gamepad1.x) {
+        // send joystick inputs to the bench
+        //testBench.elevatorPosition(100);
+        leds.goOcean();
+      }
+      if (gamepad1.y) {
+        // send joystick inputs to the bench
+        //testBench.elevatorPosition(0);
+        leds.goStrobe();
+      }
+
+      if (gamepad1.right_bumper) {
+        // send joystick inputs to the bench
+        testBench.latchPosition(1);
+      }
+      if (gamepad1.left_bumper) {
+        // send joystick inputs to the bench
+        testBench.latchPosition(0);
+      }
+
+
+      if (gamepad1.a) {
+        // send joystick inputs to the leds
+        leds.goConfetti();
+      }
+
+      if (gamepad1.b) {
+        // send joystick inputs to the leds
+        leds.goFireLarge();
+      }
+
+
+
+
+
+
+
+    }
+  }
+}
